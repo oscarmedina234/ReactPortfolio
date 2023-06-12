b06582de8ff7998e21c84d11ba8764a1182fc979
oscarmedina234 <oscarmedina234@gmail.com>
2023-06-11 21:00:11 -0500

    inserted template code and css to edit

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
-import { GridAnimation } from './components/Bubble'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar/index";
+import { Banner } from "./components/Banner/index";
+import { Skills } from "./components/Skills/index";
+import { Projects } from "./components/ProjectsSection/index";
+import { Contact } from "./components/Contact/index";
+import { Footer } from "./components/Footer/index";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
