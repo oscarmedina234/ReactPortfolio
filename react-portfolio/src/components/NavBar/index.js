42be894685aed1cd4b2756f4417d001c8234514b
oscarmedina234 <oscarmedina234@gmail.com>
2023-06-21 12:12:04 -0500

    changes made

--- react-portfolio/src/components/NavBar/index.js
+++ react-portfolio/src/components/NavBar/index.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import './style.css';
 import { Navbar, Nav, Container } from "react-bootstrap";
 // import logo from '../assets/img/logo.svg';
-// import navIcon1 from '../assets/img/nav-icon1.svg';
-// import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon1 from '../../assets/github-mark.png';
+import navIcon2 from '../../assets/linkedin.png';
 // import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
@@ -51,9 +51,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                {/* <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a> */}
+                <a href="https://github.com/oscarmedina234"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/oscar-medina-82b771265/"><img src={navIcon2} alt="" /></a>
+                {/* <a href="#"><img src={navIcon3} alt="" /></a> */}
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
